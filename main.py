994dc302407a1cd99e669c1e558be1682745a2d9
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-23 22:11:14 -0600

    define a mthod to checkout a book

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 class Book:
     def __init__(self, title, series, author):
         self._title = title
@@ -8,6 +10,11 @@ class Book:
     def __repr__(self):
         return f"{self._title} by {self._author}"
 
+    def checkout(self, checked_out_on=data.today()):
+        """
+        method to checkout a book
+        """
+        self._checked_out_on = checked_out_on
 
 fellowship_of_the_ring = Book(
     "The Fellowship of the Ring",
