d8154646bfc402eb6e05d7e031a6960814bb17cc
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-23 22:01:15 -0600

    making a book class and some books

---
+++ main.py
@@ -0,0 +1,23 @@
+class Book:
+    def __init__(self, title, series, author):
+        self._title = title
+        self._series = series
+        self._author = author
+
+    def __repr__(self):
+        return f"{self._title} by {self._author}"
+
+
+fellowship_of_the_ring = Book(
+    "The Fellowship of the Ring",
+    "The Lord of the Rings",
+    "J.R.R Tolkien"
+)
+
+grapes_of_wrath = Book(
+    "The Grapes of Wrath",
+    None,
+    "John Steinbeck"
+)
+
+print(fellowship_of_the_ring)
